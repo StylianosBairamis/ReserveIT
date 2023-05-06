d47a067e4fc4fec8d8f46ba3629c14dd4e13066e
StylianosBairamis <smpairam@csd.auth.gr>
2023-05-06 14:04:26 +0300

    created Place,Reservation Table

--- app/src/main/java/com/example/androidergasia/MainActivity.java
+++ app/src/main/java/com/example/androidergasia/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.androidergasia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //RecyclerView recyclerView = findViewById(R.id.RecycleView);
+
+       // LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+
+       // recyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    public void addPlace(View view)
+    {
+        DBhandler dBhandler = new DBhandler(this,null,null,1);
+        Place place = new Place(0,"sindos","polu kalo",5.5);
+        dBhandler.addPlace(place);
     }
 }
