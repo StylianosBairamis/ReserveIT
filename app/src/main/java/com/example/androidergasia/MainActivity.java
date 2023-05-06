bd110703bb7f0df34df2d53903b8476728000764
StylianosBairamis <smpairam@csd.auth.gr>
2023-05-06 22:41:20 +0300

    some recyclerView and db changes

--- app/src/main/java/com/example/androidergasia/MainActivity.java
+++ app/src/main/java/com/example/androidergasia/MainActivity.java
@@ -4,10 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity
+{
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter<RecyclerAdapter.ViewHolder> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -15,17 +28,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //RecyclerView recyclerView = findViewById(R.id.RecycleView);
+        RecyclerView recyclerView1 = findViewById(R.id.recycler_View);
 
-       // LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
 
-       // recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView1.setLayoutManager(linearLayoutManager);
+
+        DBhandler db = new DBhandler(this,null,null,1);
+
+//Set my Adapter for the RecyclerView
+        adapter = new RecyclerAdapter(db);
+        recyclerView1.setAdapter(adapter);
+
+//        Resources res = getResources();
+//        int resourceId = R.drawable.first;
+//        Bitmap bitmap = BitmapFactory.decodeResource(res, resourceId);
+//
+//        try {
+//            DBhandler.test(bitmap);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+
+    }
+
+
+
+    public void test(Bitmap myBitmap) throws IOException
+    {
+//        FileOutputStream outputStream = openFileOutput("my_image.jpg", Context.MODE_PRIVATE);
+//        myBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+//        outputStream.close();
     }
 
     public void addPlace(View view)
     {
-        DBhandler dBhandler = new DBhandler(this,null,null,1);
-        Place place = new Place(0,"sindos","polu kalo",5.5);
-        dBhandler.addPlace(place);
+        DBhandler db = new DBhandler(this,null,null,1);
+
+        Place place = new Place(0,"The Hungry Wolf","polu kalo","Cafe");
+
+        db.addPlace(place);
+        place = new Place(0,"La Cucina Bella","polu kalo","Cafe");
+        db.addPlace(place);
+
+
+        place = new Place(0,"The Spice Route","polu kalo","Bar");
+        db.addPlace(place);
+
+
+        place = new Place(0,"The Sizzling Pan","polu kalo","Bar");
+        db.addPlace(place);
+
+
+        place = new Place(0,"The Fork & Knife","polu kalo","Restaurant");
+        db.addPlace(place);
+
     }
+
 }
